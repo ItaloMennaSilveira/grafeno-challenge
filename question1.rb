7a58a683904a7ce08bf1a507741e981226de244d
ItaloMennaSilveira <italo.silveira@nave.rs>
2022-07-09 09:31:28 -0300

    fix: question1

--- question1.rb
+++ question1.rb
@@ -1,25 +1,11 @@
 # frozen_string_literal: true
 
-require 'byebug'
-
-input = [
-  { name: 'Maria Neusa de Aparecida',
-  cpf: '97905796671',
-  state: 'Sao Paulo',
-  value: '1234' },
-  { name: 'Ricardo Fontes',
-  cpf: '44010762900',
-  state: 'Rio Grande do Sul',
-  value: '567' }
-  ]
-
 class Question1
   ALLOWED_FIELDS = %i[name cpf state value].freeze
   MAX_FIELD_LENGTH = 10
   START_FILL_INDEX = 11
 
   def solucao(array_of_objects)
-    byebug
     sanitize_objects(array_of_objects)
   end
 
@@ -43,5 +29,3 @@ class Question1
     string[0..MAX_FIELD_LENGTH].ljust(START_FILL_INDEX, ' ')
   end
 end
-
-p Question1.new.solucao(input) == "Maria Neusa97905796671Sao Paulo  1234       \nRicardo Fon44010762900Rio Grande 567        "
